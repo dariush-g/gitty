f4f936d1646f8d6ca012aac14bb8b4203e7be49f
dariush-g <dariush@ghassemieh.com>
2026-09-15 14:41:54 -0700

    (GP-1.1): Initialized Project

--- Gitty.java
+++ Gitty.java
@@ -1,8 +1,18 @@
+import java.io.IOException;
 
-
-void main(String[] args) {
-    switch (args[1]) {
+void main(String[] args) throws IOException {
+    if (args.length == 0)
+        return;
+    switch (args[0]) {
         case "init":
+            Path path = Paths.get(".gitty");
+            String currentPath = System.getProperty("user.dir");
+            if (Files.isDirectory(path))
+                System.out.println("Reitialized empty Gitty repository in " + currentPath);
+
+            Files.createDirectories(path);
+            System.out.println("Initialized empty Gitty repository in " + currentPath);
+            Files.createDirectories(Paths.get("./.gitty/objects/"));
         case "add":
         case "commit":
     }
