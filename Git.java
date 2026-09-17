1e877bfafc8724ebe2dd0b2df752b110ab98993b
dariush-g <dariush@ghassemieh.com>
2026-09-17 10:57:13 -0700

    (GP-2.1): Program Can Initialize Repository

--- Git.java
+++ Git.java
@@ -4,22 +4,28 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Git {
-
     void main(String[] args) throws IOException {
         if (args.length == 0)
             return;
         switch (args[0]) {
             case "init":
-                Path path = Paths.get(".gitty");
-                String currentPath = System.getProperty("user.dir");
-                if (Files.isDirectory(path))
-                    System.out.println("Reitialized empty Gitty repository in " + currentPath);
-
-                Files.createDirectories(path);
-                System.out.println("Initialized empty Gitty repository in " + currentPath);
-                Files.createDirectories(Paths.get("./.gitty/objects/"));
+                init();
             case "add":
             case "commit":
+        }
+    }
+
+    void init() throws IOException {
+        Path path = Paths.get("git/");
+        String currentPath = System.getProperty("user.dir");
+        if (Files.isDirectory(path))
+            System.out.println("Reitialized empty Gitty repository in " + currentPath);
+        else {
+            Files.createDirectories(path);
+            System.out.println("Initialized empty Gitty repository in " + currentPath);
+            Files.createDirectories(Paths.get("./git/objects/"));
+            Files.createDirectories(Paths.get("./git/index/"));
+            Files.createFile(Paths.get("./git/HEAD"));
         }
     }
 }
