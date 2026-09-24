b5048a1b7be8a0bc6e3f44662be89afdfefb03fd
dariush-g <dariush@ghassemieh.com>
2026-09-24 14:57:13 -0700

    end of class

--- Git.java
+++ Git.java
@@ -22,14 +22,17 @@ public class Git {
             case "commit":
                 break;
         }
-
-        System.out.println();
     }
 
     void add(String file_name) throws IOException {
         var hash = FileHasher.hashFile(file_name);
-        Files.write(Path.of("./git/objects/" + hash), Files.readAllBytes(Paths.get(file_name)));
-        Files.writeString(Path.of("./git/index"), hash + " " + file_name + "\n", StandardOpenOption.APPEND);
+        var path = Path.of("./git/objects/" + hash);
+        if (Files.exists(path)) {
+
+        }
+        Files.write(path, Files.readAllBytes(Paths.get(file_name)));
+        Files.writeString(Path.of("./git/index"), hash + " " + file_name + "\n",
+                StandardOpenOption.APPEND);
     }
 
     void init() throws IOException {
